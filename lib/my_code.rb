7eab3fb4d518651a7e360a7c3e972602ac7cdd78
Ruari Day <ruariday@gmail.com>
2020-01-09 12:27:32 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -3,6 +3,7 @@ def map_to_negativize(source_array)
   i = 0 
   while i < source_array.length do 
     negativized_array << (source_array[i] * -1)
-    
+    i += 1 
   end 
+  negativized_array
 end
