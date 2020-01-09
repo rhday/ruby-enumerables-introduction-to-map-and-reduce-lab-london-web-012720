b33c6ab0e4f87a584e4d71fd89d7d994a049bfa3
Ruari Day <ruariday@gmail.com>
2020-01-09 12:40:53 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -48,5 +48,9 @@ def reduce_to_total(source_array, starting_point)
   total
 end
 
-def
+def reduce_to_all_true(source_array)
+  true_array = []
+  i = 0 
+  while i < source_array.length do 
+  end
 end
